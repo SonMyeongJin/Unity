4c14b795ff0b2691cf4a54f30f99146945553e47
SonMyeongJin <smj2802@naver.com>
2023-11-20 02:28:00 +0900

    적 기본변수 설정

---
+++ enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    public static int kill = 0;
+    public float MaxHealth;
+    public float CurHealth;
+    public int attack;
+
+
+}
