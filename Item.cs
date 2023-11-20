92a69bb4635350d7a09fecd1917c6998c0c94d7f
SonMyeongJin <smj2802@naver.com>
2023-11-21 00:40:55 +0900

    아이템 타입설정 및 제자리 회전 로직 구현

---
+++ Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type { aGun, bGun, bag, guncase, ball1,ball2, health }
+    public Type Itemtype;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 30 * Time.deltaTime);
+    }
+}
